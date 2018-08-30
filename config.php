a51ac0ce55e057e2156cf240a56ce2c0467bfa16
staxDB <borndavid508@gmail.com>
2018-08-30 10:35:55 +0200

    Updated for version release

--- config.php
+++ config.php
@@ -1,21 +1,23 @@
 <?php
 
 use humhub\modules\dashboard\widgets\Sidebar;
+use humhub\modules\external_calendar\Events;
 use humhub\commands\CronController;
+use humhub\modules\external_calendar\Module;
 use humhub\commands\IntegrityController;
 
 return [
     'id' => 'external_calendar',
-    'class' => 'humhub\modules\external_calendar\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\external_calendar',
     'events' => [
-        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => ['humhub\modules\external_calendar\Module', 'onDashboardSidebarInit']],
-        ['class' => 'humhub\modules\calendar\interfaces\CalendarService', 'event' => 'getItemTypes', 'callback' => ['humhub\modules\external_calendar\Events', 'onGetCalendarItemTypes']],
-        ['class' => 'humhub\modules\calendar\interfaces\CalendarService', 'event' => 'findItems', 'callback' => ['humhub\modules\external_calendar\Events', 'onFindCalendarItems']],
-        ['class' => CronController::class, 'event' => CronController::EVENT_ON_HOURLY_RUN, 'callback' => ['humhub\modules\external_calendar\Events', 'onCronHourlyRun']],
-        ['class' => CronController::class, 'event' => CronController::EVENT_ON_DAILY_RUN, 'callback' => ['humhub\modules\external_calendar\Events', 'onCronDailyRun']],
-        ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => ['humhub\modules\external_calendar\Events', 'onIntegrityCheck']],
-        ['class' => '\humhub\modules\content\widgets\WallEntryLinks', 'event' => 'init', 'callback' => ['humhub\modules\external_calendar\Events', 'onWallEntryLinks']],
+        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Module::class, 'onDashboardSidebarInit']],
+        ['class' => 'humhub\modules\calendar\interfaces\CalendarService', 'event' => 'getItemTypes', 'callback' => [Events::class, 'onGetCalendarItemTypes']],
+        ['class' => 'humhub\modules\calendar\interfaces\CalendarService', 'event' => 'findItems', 'callback' => [Events::class, 'onFindCalendarItems']],
+        ['class' => CronController::class, 'event' => CronController::EVENT_ON_HOURLY_RUN, 'callback' => [Events::class, 'onCronHourlyRun']],
+        ['class' => CronController::class, 'event' => CronController::EVENT_ON_DAILY_RUN, 'callback' => [Events::class, 'onCronDailyRun']],
+        ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => [Events::class, 'onIntegrityCheck']],
+        ['class' => '\humhub\modules\content\widgets\WallEntryLinks', 'event' => 'init', 'callback' => [Events::class, 'onWallEntryLinks']],
     ],
 ];
 ?>
